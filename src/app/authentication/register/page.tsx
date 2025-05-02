00b36a6e86b1478296cf82510ea0334f4ffbd1d7
ChilyGarcia <jugada160324@gmail.com>
2025-05-02 11:36:50 -0500

    <feat>: Implemented register and login in different pages

--- src/app/authentication/register/page.tsx
+++ src/app/authentication/register/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import NavBar from "@/components/navbar";
+import Link from "next/link";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import { Toaster, toast } from "sonner";
-import { isDataView } from "util/types";
 
 function Checkbox({ id, className = "", ...props }) {
   return (
@@ -24,65 +24,6 @@ function Input({ className = "", ...props }) {
       className={`flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       {...props}
     />
-  );
-}
-
-const CollapsibleContext = React.createContext(undefined);
-
-function Collapsible({ open, onOpenChange, children }) {
-  return (
-    <CollapsibleContext.Provider value={{ open, onOpenChange }}>
-      <div>{children}</div>
-    </CollapsibleContext.Provider>
-  );
-}
-
-function CollapsibleTrigger({ children, className = "" }) {
-  const context = React.useContext(CollapsibleContext);
-  if (!context) {
-    throw new Error("CollapsibleTrigger must be used within Collapsible");
-  }
-
-  return (
-    <button
-      type="button"
-      className={className}
-      onClick={() => context.onOpenChange(!context.open)}
-    >
-      {children}
-    </button>
-  );
-}
-
-function CollapsibleContent({ children, className = "" }) {
-  const context = React.useContext(CollapsibleContext);
-  if (!context) {
-    throw new Error("CollapsibleContent must be used within Collapsible");
-  }
-
-  if (!context.open) {
-    return null;
-  }
-
-  return <div className={`${className}`}>{children}</div>;
-}
-
-function ChevronDown(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <polyline points="6 9 12 15 18 9"></polyline>
-    </svg>
   );
 }
 
@@ -128,8 +69,7 @@ function EyeOff(props) {
   );
 }
 
-export default function RegistroForm() {
-  const [isOpen, setIsOpen] = React.useState(false);
+export default function RegisterPage() {
   const [showPassword, setShowPassword] = React.useState(false);
   const [errors, setErrors] = React.useState<{
     email?: string;
@@ -150,31 +90,26 @@ export default function RegistroForm() {
     phone_number: "",
     phone_indicator: "+57",
   });
-  const [loginData, setLoginData] = React.useState({
-    email: "",
-    password: "",
-  });
   const [isValidated, setIsValidated] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
   const router = useRouter();
 
-  const validateField = (name, value, isLogin = false) => {
+  const validateField = (name: string, value: string) => {
     let error = "";
 
     if (name === "email" && !value.trim()) {
       error = "El correo es requerido.";
     } else if (name === "password" && !value.trim()) {
       error = "La contraseña es requerida.";
-    } else if (name === "name" && !value.trim() && !isLogin) {
+    } else if (name === "name" && !value.trim()) {
       error = "El nombre es requerido.";
     } else if (
       name === "password_confirmation" &&
-      value !== registerData.password &&
-      !isLogin
+      value !== registerData.password
     ) {
       error = "Las contraseñas no coinciden.";
-    } else if (name === "phone_number" && !value.trim() && !isLogin) {
+    } else if (name === "phone_number" && !value.trim()) {
       error = "El número de teléfono es requerido.";
     }
 
@@ -182,39 +117,25 @@ export default function RegistroForm() {
   };
 
   const validateForm = () => {
-    if (isOpen) {
-      const isValid =
-        loginData.email.trim() &&
-        loginData.password.trim() &&
-        !errors.email &&
-        !errors.password;
-      setIsValidated(isValid);
-    } else {
-      const isValid =
-        registerData.name.trim() &&
-        registerData.email.trim() &&
-        registerData.password.trim() &&
-        registerData.password === registerData.password_confirmation &&
-        Object.values(errors).every((error) => !error);
+    const isValid =
+      registerData.name.trim() &&
+      registerData.email.trim() &&
+      registerData.password.trim() &&
+      registerData.password === registerData.password_confirmation &&
+      Object.values(errors).every((error) => !error);
 
-      setIsValidated(isValid);
-    }
+    setIsValidated(!!isValid);
   };
 
-  const handleBlur = (e) => {
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    validateField(name, value, isOpen);
+    validateField(name, value);
     validateForm();
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    if (isOpen) {
-      setLoginData((prev) => ({ ...prev, [name]: value }));
-    } else {
-      setRegisterData((prev) => ({ ...prev, [name]: value }));
-    }
-
+    setRegisterData((prev) => ({ ...prev, [name]: value }));
     validateForm();
   };
 
@@ -237,10 +158,9 @@ export default function RegistroForm() {
           },
           body: JSON.stringify(modifiedRegisterData),
         }
-      ).finally(() => {
-        setIsLoading(false);
-      });
+      );
       const data = await response.json();
+      setIsLoading(false);
 
       if (!response.ok) {
         if (data.errors) {
@@ -264,69 +184,18 @@ export default function RegistroForm() {
     } catch (error: any) {
       console.error("Error registrando:", error.message);
       toast.error("Error registrando: " + error.message);
+      setIsLoading(false);
     }
   };
 
-  const fetchLogin = async () => {
-    const appointmentDetails = localStorage.getItem("appointmentDetails");
-
-    setIsLoading(true);
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(loginData),
-        }
-      ).finally(() => {
-        setIsLoading(false);
-      });
-      const data = await response.json();
-
-      console.log(data.error);
-
-      if (!response.ok) {
-        toast.error(data.error || "Error al iniciar sesion");
-        return;
-      }
-
-      toast.success("Login exitoso!");
-      Cookies.set("token", data.access_token, { expires: 1 });
-
-      if (appointmentDetails) {
-        router.push("/payments");
-      } else {
-        router.push("/");
-      }
-    } catch (error: any) {
-      console.error("Error en el login:", error.message);
-      toast.error("Error al iniciar sesión: " + error.message);
-    }
-  };
-
-  const handleRegister = () => {
-    if (isOpen) {
-      console.log("Se llama la peticion del login");
-
-      fetchLogin();
-    } else {
-      console.log("Se llama la peticion del register");
-
-      fetchRegister();
-    }
-  };
-
-  const handleInputFocus = () => {
-    setIsOpen(false);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetchRegister();
   };
 
   return (
     <>
-      <NavBar></NavBar>
-
+      <NavBar />
       <Toaster />
 
       <div className="min-h-screen bg-white p-4 md:p-6 mt-12">
@@ -337,7 +206,7 @@ export default function RegistroForm() {
               Introduce tu información correspondiente
             </p>
           </div>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <div className="space-y-2">
               <p className="text-sm font-medium text-primary">Tus datos</p>
               <Input
@@ -348,7 +217,6 @@ export default function RegistroForm() {
                 value={registerData.name}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                onFocus={handleInputFocus}
               />
               {errors.name && (
                 <p className="text-sm text-red-500">{errors.name}</p>
@@ -361,7 +229,6 @@ export default function RegistroForm() {
                 value={registerData.email}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                onFocus={handleInputFocus}
               />
               {errors.email && (
                 <p className="text-sm text-red-500">{errors.email}</p>
@@ -375,7 +242,6 @@ export default function RegistroForm() {
                   value={registerData.phone_number}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  onFocus={handleInputFocus}
                 />
                 {errors.phone_number && (
                   <p className="text-sm text-red-500">{errors.phone_number}</p>
@@ -390,7 +256,6 @@ export default function RegistroForm() {
                   value={registerData.password}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  onFocus={handleInputFocus}
                 />
                 {errors.password && (
                   <p className="text-sm text-red-500">{errors.password}</p>
@@ -410,13 +275,12 @@ export default function RegistroForm() {
               <div className="relative">
                 <Input
                   className="rounded-full border-primary px-4 py-2 pr-10"
-                  placeholder="Confirma tu ontraseña"
+                  placeholder="Confirma tu contraseña"
                   type={showPassword ? "text" : "password"}
                   name="password_confirmation"
                   value={registerData.password_confirmation}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  onFocus={handleInputFocus}
                 />
                 {errors.password_confirmation && (
                   <p className="text-sm text-red-500">
@@ -437,50 +301,6 @@ export default function RegistroForm() {
               </div>
             </div>
 
-            <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-              <CollapsibleTrigger className="flex w-full items-center justify-between rounded-full border border-primary px-4 py-2">
-                <span className="text-primary opacity-50">Iniciar Sesión</span>
-                <ChevronDown
-                  className={`h-4 w-4 text-primary opacity-50 transition-transform ${
-                    isOpen ? "rotate-180" : ""
-                  }`}
-                />
-              </CollapsibleTrigger>
-              <CollapsibleContent className="space-y-2 rounded-xl bg-white p-4 shadow-lg mt-2">
-                <Input
-                  className="rounded-full border-primary px-4 py-2"
-                  placeholder="Correo Electrónico"
-                  type="email"
-                  name="email"
-                  value={loginData.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <div className="relative">
-                  <Input
-                    className="rounded-full border-primary px-4 py-2 pr-10"
-                    placeholder="Contraseña"
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    value={loginData.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword(!showPassword)}
-                    className="absolute right-3 top-1/2 -translate-y-1/2 text-primary opacity-50"
-                  >
-                    {showPassword ? (
-                      <EyeOff className="h-4 w-4" />
-                    ) : (
-                      <Eye className="h-4 w-4" />
-                    )}
-                  </button>
-                </div>
-              </CollapsibleContent>
-            </Collapsible>
-
             <div className="flex items-center space-x-2">
               <Checkbox
                 id="terms"
@@ -494,26 +314,28 @@ export default function RegistroForm() {
               </label>
             </div>
             <button
+              type="submit"
               className={`inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 w-full rounded-full px-4 py-2 ${
                 isValidated
                   ? "bg-primary text-white hover:bg-primary/90"
                   : "bg-gray-300 text-gray-500 cursor-not-allowed"
               }`}
-              onClick={(e) => {
-                e.preventDefault();
-                handleRegister();
-              }}
               disabled={!isValidated}
             >
               {isLoading ? (
                 <span className="loading loading-spinner loading-md"></span>
-              ) : isOpen ? (
-                <label>Iniciar sesión</label>
               ) : (
-                <label>Registrarse</label>
+                "Registrarse"
               )}
             </button>
           </form>
+
+          <p className="text-center text-sm text-primary">
+            ¿Ya tienes cuenta?{" "}
+            <Link href="/login" className="font-medium underline">
+              Iniciar sesión
+            </Link>
+          </p>
         </div>
       </div>
     </>
