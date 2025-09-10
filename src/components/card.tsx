16ba21f8773ee3593ee84dc74916d832156706e7
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-09-10 16:45:10 -0500

    fix:fix-card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,7 +12,7 @@ export default function CardHero({
     <div
       className={`flex ${reverse ? "md:flex-col" : "md:flex-col"} ${
         reverse ? "flex-row-reverse" : "flex-row"
-      } items-center bg-white shadow-xl border border-gray-100 rounded-xl overflow-hidden w-[350px] sm:w-[380px] h-[230px] md:w-[300px] md:h-[400px] hover:shadow-2xl transition-all duration-300`}
+      } items-center bg-white shadow-xl border border-gray-100 rounded-xl overflow-hidden w-[350px] sm:w-[380px] h-[230px] md:w-[300px] md:h-[400px] hover:shadow-2xl transition-all duration-300 relative`}
     >
       <div className="md:hidden flex-shrink-0 w-1/2 h-full relative">
         <Image
@@ -25,25 +25,23 @@ export default function CardHero({
         />
       </div>
 
-      <div className="flex-1 h-full flex flex-col justify-center items-center text-center p-3 sm:p-4 md:p-6">
+      <div className="flex-1 h-full flex flex-col justify-start items-center text-center p-3 sm:p-4 md:px-6 md:pt-6 md:pb-2">
         <h3 className="text-lg font-extrabold text-[#0C0CAA] mb-2 md:mb-3">
           {title}
         </h3>
-        <p className="text-xs sm:text-sm text-gray-700 overflow-hidden md:mb-4">
+        <p className="text-xs sm:text-sm text-gray-700 overflow-hidden md:pb-1">
           {description}
         </p>
-        <button className="mt-3 sm:mt-4 bg-[#0C0CAA] text-white py-1.5 sm:py-2 px-6 sm:px-8 rounded-full text-xs sm:text-sm font-medium hover:bg-blue-800 transition-colors md:ml-2 md:self-start">
-          {buttonText}
-        </button>
       </div>
 
-      <div className="hidden md:flex flex-1 items-center justify-center p-4 w-full">
+      <div className="hidden md:flex absolute bottom-0 right-0 w-3/4 h-3/4 p-0 overflow-visible">
         <div className="relative w-full h-full">
           <Image
             src={imageSrc}
             alt={title}
             fill
-            className="object-contain mx-auto"
+            className="object-contain object-right-bottom"
+            sizes="(max-width: 768px) 100vw, 50vw"
           />
         </div>
       </div>
