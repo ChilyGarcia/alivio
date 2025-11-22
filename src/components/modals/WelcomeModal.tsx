6f1aef05afeaec0d3b6d3c47f74e91cb4f997dc3
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-11-22 15:33:23 -0500

    fix:fix-image-welcome-modal

--- src/components/modals/WelcomeModal.tsx
+++ src/components/modals/WelcomeModal.tsx
@@ -9,10 +9,10 @@ const WelcomeModal = () => {
 
   useEffect(() => {
     // Verificar si ya se mostró el modal
-    const hasSeenModal = localStorage.getItem("hasSeenWelcomeModal");
+    const hasSeenModal = localStorage.getItem("hasSeenWelcomeModal9");
     if (!hasSeenModal) {
       setIsOpen(true);
-      localStorage.setItem("hasSeenWelcomeModal", "true");
+      localStorage.setItem("hasSeenWelcomeModal9", "true");
     }
   }, []);
 
@@ -42,11 +42,10 @@ const WelcomeModal = () => {
             />
           </svg>
         </button>
-        {/* Imagen */}
-        <div className="w-full md:w-1/2 flex items-center justify-center p-8">
-          <div className="relative w-48 h-48 md:w-56 md:h-56 rounded-full overflow-hidden border-4 border-white shadow-lg">
+        <div className="w-full md:w-1/2 flex items-start justify-center pt-12 md:pt-16">
+          <div className="relative w-56 h-56 md:w-64 md:h-64 rounded-full overflow-hidden">
             <Image
-              src="/images/doc1.png"
+              src="/images/Heros.png"
               alt="Bienvenido a AliviApp"
               fill
               className="object-cover"
